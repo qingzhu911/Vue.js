38e6748052881d1d60394c018086096ef0fca3e2
impor File <825560019@qq.com>
2017-11-06 21:15:30 +0800

    lesson-6

---
+++ lesson-6/app.js
@@ -0,0 +1,35 @@
+new Vue({
+	el:"#vue-app",
+	data:{
+		err:false,
+		succ:false
+	},
+	methods:{
+		
+	},
+	computed:{
+
+	}
+});
+/*
+	v-if:为假时候dom结构中不存在
+	v-show:为假dom结构中存在
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
