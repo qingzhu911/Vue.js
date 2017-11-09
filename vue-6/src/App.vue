9caae233cc946938bc7f6202a8816b09075e28be
impor File <825560019@qq.com>
2017-11-09 22:15:33 +0800

    vue-6

---
+++ vue-6/src/App.vue
@@ -0,0 +1,63 @@
+<!-- template:是用到的模板,模板中,有且只能有一个根标签 -->
+<template>
+  <div id="app">
+    <!-- 4 -->
+    <app-header v-on:titleChange="updateTitle($event)" v-bind:title="title"></app-header>
+    <app-users v-bind:users="users"></app-users>
+    <app-footer v-bind:title="title"></app-footer>
+  </div>
+</template>
+
+<!-- 行为:
+  script:标签中,囊括的是所有的属性和方法
+  export default{} :对外公开当前文件可被其他文件引入
+  name:给当前组件起名,此名字可以在其他组件中以标签的形式调用
+  components:承载所有当前组件需要依赖的其他组件
+ -->
+
+<script>
+  // 2
+  import Header from './components/Header'
+  import Users from './components/Users'
+  import Footer from './components/Footer'
+
+  export default {
+    name: 'app',
+    // 3.注册
+    components: {
+      "app-header": Header,
+      "app-users": Users,
+      "app-footer": Footer
+
+    },
+    data() {
+      return {
+        users: [
+          {name: "MiaoJie", position: "Web开发", show: false},
+          {name: "MiaoJie", position: "Web开发", show: false},
+          {name: "MiaoJie", position: "Web开发", show: false},
+          {name: "MiaoJie", position: "Web开发", show: false},
+          {name: "MiaoJie", position: "Web开发", show: false},
+          {name: "MiaoJie", position: "Web开发", show: false},
+          {name: "MiaoJie", position: "Web开发", show: false}
+        ],
+        title: "AngularJS"
+      }
+    },
+    methods: {
+      updateTitle: function (str) {
+        this.title = str;
+      }
+    },
+    computed: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
+
+
+
+
+
