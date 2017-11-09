f6c9a64642eaf4f3ac6c77d407e0302805e11bc1
impor File <825560019@qq.com>
2017-11-09 22:06:22 +0800

    vue-3

---
+++ vue-3/src/components/Users.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="users">
+    <h1>hello Users</h1>
+   <ul>
+     <li v-for="user in users">{{user}}</li>
+   </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+       users:["zhangsan","lisi","wangwu"]
+      }
+    },
+    methods:{
+     
+    },
+    computed:{
+
+    }
+  }
+</script>
+
+<style scoped>
+  h1{
+    color: red;
+  }
+</style>
